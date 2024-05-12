879aa3d66cdda9699004eca13a1253a0bfa5601a
Alexander Golyshev <youcanwinmyheart@gmail.com>
2024-05-12 03:25:37 +0300

    fix

---
+++ ConceptualSystem_WPF_MVVM/View/UserControls/Concepts.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConceptualSystem_WPF_MVVM.View.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для Concepts.xaml
+    /// </summary>
+    public partial class Concepts : UserControl
+    {
+        public Concepts()
+        {
+            InitializeComponent();
+
+            List<string> conceptsWords = new List<string>
+            {
+                "Паспорт",
+                "Человек",
+                "Компьютер",
+                "Ноутбук",
+            };
+            conceptsList.ItemsSource = conceptsWords;
+
+            List<string> structureConceptWords = new List<string>
+            {
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+                "Фамилия",
+                "Имя",
+                "Отчество",
+                "Возраст",
+            };
+            structureConceptList.ItemsSource = structureConceptWords;
+
+        }
+    }
+}
